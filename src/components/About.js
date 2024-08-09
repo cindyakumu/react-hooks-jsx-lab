590cf137d5e3962094b94f6e44800ae9e658ed26
Cindy <cindyakumu40@gmail.com>
2024-08-09 23:43:53 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Ensure the image is correctly imported
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am passionate about web development and love creating new projects.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
